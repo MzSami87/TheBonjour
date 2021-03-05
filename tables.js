a01572bab55e429511dacf32ef3f5155a0b2d777
Samantha <71792921+MzSami87@users.noreply.github.com>
2021-03-05 03:17:23 -0600

    gh-pages

--- tables.js
+++ tables.js
@@ -1,4 +1,4 @@
-var xhr = new XMLHttpRequest();
+let xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
     if(xhr.readyState === 4) {
     var tables = JSON.parse(xhr.responseText);
